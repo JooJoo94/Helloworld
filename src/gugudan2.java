158f380faa3e28598aa6f42bbf331fa82bd58c8d
USER <USER@USER-SRBCUPKCJA>
2019-07-25 15:59:38 +0900

    구구단(변수)

---
+++ src/gugudan2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class gugudan2 {
+	public static void main(String[] args) {
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner sc = new Scanner(System.in);
+		int num =  sc.nextInt();
+		System.out.println("number :" + num);
+		
+		int result = num*1;
+		System.out.println(result);
+		
+		result = num*2;
+		System.out.println(result);
+		
+		result = num*3;
+		System.out.println(result);
+		
+		result = num*4;
+		System.out.println(result);
+		
+		result = num*5;
+		System.out.println(result);
+		
+		result = num*6;
+		System.out.println(result);
+		
+		result = num*7;
+		System.out.println(result);
+		
+		result = num*8;
+		System.out.println(result);
+		
+		result = num*9;
+		System.out.println(result);
+		
+		
+	}
+
+}
